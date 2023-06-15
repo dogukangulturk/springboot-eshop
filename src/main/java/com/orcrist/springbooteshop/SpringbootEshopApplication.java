3bc18bb0b43d39f56d8b25c2d87a35430e967685
Doğukan Gültürk <91672770+dogukangulturk@users.noreply.github.com>
2023-06-15 13:12:46 +0300

    Initial commit

---
+++ src/main/java/com/orcrist/springbooteshop/SpringbootEshopApplication.java
@@ -0,0 +1,13 @@
+package com.orcrist.springbooteshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orcrist/springbooteshop/SpringbootEshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orcrist.springbooteshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
